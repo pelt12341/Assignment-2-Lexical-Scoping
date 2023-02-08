3917604bb940634517627a760b46b3394951e2b1
pelt12341 <pabloleon360@gmail.com>
2023-02-07 22:05:30 -0300

    new version

---
+++ Assignment2.R
@@ -0,0 +1,30 @@
+## Answer to Assignment 2: Lexical Scoping
+
+
+# To save the inverse of a given matrix
+makeCacheMatrix2 <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve)s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+##
+## To get the inverse from cache 
+cacheSolve2 <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
